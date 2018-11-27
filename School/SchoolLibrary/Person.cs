0851c3736af6ba0e93ca65c598fbcb7b2e643d0d
Daniel Buchanan <daniel@getmorepc.com>
2018-11-27 09:40:45 -0500

    virtual method addition
    added to person class

--- School/SchoolLibrary/Person.cs
+++ School/SchoolLibrary/Person.cs
@@ -11,5 +11,16 @@ namespace SchoolLibrary
         public string Email { get; set; }
 
         public abstract float ComputeGradeAverage();
+
+        public virtual string SendMessage(string message)
+        {
+            var sb = new StringBuilder();
+            var timeStamp = string.Format("Sent on {0:D} at {0:t}", DateTime.Now);
+            sb.AppendLine(timeStamp);
+            sb.AppendLine("");
+            sb.AppendLine("Dear " + FirstName + ",");
+            sb.AppendLine(message);
+            return sb.ToString();
+        }
     }
 }
